b05ddb611ba94720f9d8d292e7ccc54c541cec2f
Mikael KUREK <kurek@intuiface.com>
2021-07-01 14:46:53 +0200

    publish on release and fix bug of reflect-metadata

--- libs/core/src/lib/decorators/action.ts
+++ libs/core/src/lib/decorators/action.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import 'reflect-metadata';
 
 /**
  * Options when use action decorator.
--- libs/core/src/lib/decorators/trigger.ts
+++ libs/core/src/lib/decorators/trigger.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import 'reflect-metadata';
 
 /**
  * Options when use trigger decorator.
