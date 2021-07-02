478cf90fb949bc6a1ec22e12453dfa811c1abf26
Mikael KUREK <kurek@intuiface.com>
2021-07-02 11:11:06 +0200

    add Computor decorator

--- libs/core/src/public-api.ts
+++ libs/core/src/public-api.ts
@@ -3,6 +3,7 @@ export { Trigger } from './lib/decorators/trigger';
 export { Action } from './lib/decorators/action';
 export { Parameter } from './lib/decorators/parameter';
 export { Asset, Collection } from './lib/decorators/element.decorator';
+export { Computor } from './lib/decorators/computor';
 export { IntuifaceElement } from './lib/elements/element';
 export { ElementContainer } from './lib/elements/container';
 export { IntuifaceCollection, ICollectionConfiguration } from './lib/elements/collection';
